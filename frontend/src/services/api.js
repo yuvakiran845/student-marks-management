b40177a1636a1fa33da016e812de09ff56abded9
yuvakiran845 <yuvak2484@gmail.com>
2026-01-16 18:14:44 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5002/api';
+const API_BASE_URL = 'https://student-marks-management-1p7k.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
